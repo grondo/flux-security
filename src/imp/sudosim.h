98d9783e8050479f9b5844bae6a39142fac94b00
Mark A. Grondona <mark.grondona@gmail.com>
2018-01-17 10:46:38 -0800

    sudosim: add sudo_is_active and sudo_user_name

--- src/imp/sudosim.h
+++ src/imp/sudosim.h
@@ -25,6 +25,18 @@
 #ifndef HAVE_SUDOSIM_H
 #define HAVE_SUDOSIM_H 1
 
+#include <stdbool.h>
+
+/*  If current process was run under sudo, return the user name as recorded
+ *   in the SUDO_USER environment variable. Return NULL otherwise.
+ */
+const char * sudo_user_name (void);
+
+/*  Return true if it appears the current process is running under sudo, i.e.
+ *   real UID is 0, and SUDO_USER environment variable is set.
+ */
+bool sudo_is_active (void);
+
 /*  If running under sudo by evidence of real UID == 0 and SUDO_USER set,
  *   adapt process credentials to simulate a setuid program. That is,
  *   set the real UID/GID to that of SUDO_USER and leave effective/saved
